315881d80cec16c412e8b2e956e632efffeca1f5
Yovana Pelayo <yovana_pelayo@yahoo.com>
2023-12-09 23:53:23 -0800

    added header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        This is the header
+      </header>
+    </div>
+  );
+}
+
+export default App;
